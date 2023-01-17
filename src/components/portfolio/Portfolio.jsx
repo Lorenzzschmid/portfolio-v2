2fad9b2a3bad4fa7b29dd9c3aba6d015179c0e02
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-17 13:52:38 +0100

    portfolio-object added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,51 @@
 import React from 'react'; 
 import './portfolio.scss'; 
 
+const data = [
+    {
+      id: 1,
+      //image: IMG1,
+      title: 'Space App',
+      github: 'https://github.com',
+      demo: 'https://dribble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+    },
+    {
+      id: 2,
+    //   image: IMG2,
+      title: 'JWT-Verification',
+      github: 'https://github.com',
+      demo: 'https://dribble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    },{
+      id: 3,
+    //   image: IMG3,
+      title: 'Spotify Clone',
+      github: 'https://github.com',
+      demo: 'https://dribble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+    },{
+      id: 4,
+    //   image: IMG4,
+      title: 'Netflix Clone',
+      github: 'https://github.com',
+      demo: 'https://dribble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
+    },{
+      id: 5,
+    //   image: IMG5,
+      title: 'Movie List',
+      github: 'https://github.com',
+      demo: 'https://dribble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    },{
+      id: 6,
+    //   image: IMG6,
+      title: 'Restaurants',
+      github: 'https://github.com',
+      demo: 'https://dribble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    }
+  ]
+
 const Portfolio = () => {
   return (
     <div className="portfolio-container">
-
+        
     </div>
   )
 }
