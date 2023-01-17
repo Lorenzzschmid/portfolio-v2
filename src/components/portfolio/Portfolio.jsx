8335ca99476eeb863b42c77e22c69f2cdb3721f0
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-17 14:03:27 +0100

    images imported

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,12 @@
 import React from 'react'; 
 import './portfolio.scss'; 
+import IMG1 from '../../assets/images/IMG1.png'; 
+
 
 const data = [
     {
       id: 1,
-      //image: IMG1,
+      image: IMG1,
       title: 'Space App',
       github: 'https://github.com',
       demo: 'https://dribble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
@@ -45,7 +47,29 @@ const data = [
 const Portfolio = () => {
   return (
     <div className="portfolio-container">
-        
+        <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+      {
+        data.map(({id, image, title, github, demo}) => {
+          return (
+            <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+            <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
+            <a href={demo} className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
+          </div>
+        </article>        
+          )
+        })
+      }
+      </div>      
+    </section>
     </div>
   )
 }
