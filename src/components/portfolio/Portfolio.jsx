d435d2c0356155def6ad17d7bc3d847b53cdb74f
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-18 14:13:22 +0100

    portfolio component changed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,38 +9,38 @@ const data = [
       image: IMG1,
       title: 'Space App',
       github: 'https://github.com',
-      demo: 'https://dribble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+      demo: '#'
     },
     {
       id: 2,
     //   image: IMG2,
       title: 'JWT-Verification',
       github: 'https://github.com',
-      demo: 'https://dribble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+      demo: '#'
     },{
       id: 3,
     //   image: IMG3,
       title: 'Spotify Clone',
       github: 'https://github.com',
-      demo: 'https://dribble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+      demo: '#'
     },{
       id: 4,
     //   image: IMG4,
       title: 'Netflix Clone',
       github: 'https://github.com',
-      demo: 'https://dribble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
+      demo: '#'
     },{
       id: 5,
     //   image: IMG5,
       title: 'Movie List',
       github: 'https://github.com',
-      demo: 'https://dribble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+      demo: '#'
     },{
       id: 6,
     //   image: IMG6,
       title: 'Restaurants',
       github: 'https://github.com',
-      demo: 'https://dribble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+      demo: '#'
     }
   ]
 
@@ -55,16 +55,7 @@ const Portfolio = () => {
       {
         data.map(({id, image, title, github, demo}) => {
           return (
-            <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={image} alt={title} />
-          </div>
-          <h3>{title}</h3>
-          <div className="portfolio__item-cta">
-            <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
-            <a href={demo} className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a>
-          </div>
-        </article>        
+                   
           )
         })
       }
