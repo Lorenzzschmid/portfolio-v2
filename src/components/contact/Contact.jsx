646146dac528feb0d1127e404eef3b47cfe4598c
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-19 23:13:31 +0100

    form and emailjs event-handler added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,6 +4,23 @@ import {useRef} from 'react';
 import emailjs from 'emailjs-com'; 
 
 const Contact = () => {
+  const form = useRef(); 
+
+  const form = useRef(); 
+
+  const sendEmail = (e) => {
+    e.preventDefault(); 
+
+    emailjs
+      .sendForm(
+        'service_29g1tdc',
+        'template_zxga5no', 
+        form.current,
+        'WHQ8C8y1oGEgjvacS'
+      )
+    e.target.reset()
+  };
+  
   return (
     <div className="contact-container">
 
