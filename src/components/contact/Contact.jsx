6a8ee46df2206c06f94c0a7f72fe9cd18c3ae92f
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-20 20:11:57 +0100

    emailjs added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './contact.scss';
 import {useRef} from 'react';
-import emailjs from 'emailjs-com'; 
+import emailjs from 'emailjs-com' 
 import { BsSignal } from "react-icons/bs";
 
 const Contact = () => {
