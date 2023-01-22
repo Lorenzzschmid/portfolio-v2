c878931fb829bd121596f84bff719f1263c4c6f1
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-22 22:35:48 +0100

    stack styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Contact from './components/contact/Contact';
 import Home from './components/home/Home.jsx';
 import Nav from './components/nav/nav';
 import Portfolio from './components/portfolio/Portfolio';
+import Stack from './components/stack/Stack';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Nav />
       <About />
+      <Stack />
       <Portfolio />
       <Contact />      
     </div>
--- src/components/stack/Stack.jsx
+++ src/components/stack/Stack.jsx
@@ -4,10 +4,14 @@ import React from 'react'
 
 const Stack = () => {
   return (
-    <section className="experience">
-        <h5>My Languages, Frameworks, Libraries</h5>
-        <h2>1 Year Experience at DCI - Berlin, Codingprojects, Teamwork, </h2>
-    </section>
+    <div className="stack-container">
+        <section className="experience">
+            <h2>My Languages, Frameworks, Libraries</h2>
+            <h5>1 Year Experience at DCI - Berlin, Codingprojects, Teamwork, Agile Development</h5>
+    
+        </section>  
+    </div>
+    
   )
 }
 
