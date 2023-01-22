759e29d6a664b9c326a5e2522beccaf6393876df
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-22 22:27:26 +0100

    stack component created

---
+++ src/components/stack/Stack.jsx
@@ -0,0 +1,14 @@
+import './stack.scss';
+
+import React from 'react'
+
+const Stack = () => {
+  return (
+    <section className="experience">
+        <h5>My Languages, Frameworks, Libraries</h5>
+        <h2>1 Year Experience at DCI - Berlin, Codingprojects, Teamwork, </h2>
+    </section>
+  )
+}
+
+export default Stack
