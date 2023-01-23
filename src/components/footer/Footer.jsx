65ca267373e01cbcf5dcc4b56070b4c57c533709
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-23 22:16:30 +0100

    footer base styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,26 @@ import {SiTryhackme} from 'react-icons/si';
 const Footer = () => {
   return (
     <footer>
-      Hellau
+      <a href="#" className='footer__logo'>Lorenz Schmid</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#Experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#sources">Sources</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://twitter.com"><AiFillTwitterSquare/></a>
+        <a href="https://www.codewars.com/users/Mighty%20Lo"><SiCodewars/></a>
+        <a href="https://tryhackme.com/profile"><SiTryhackme/></a>
+      </div>
+      
+      <div className="footer__copyright">
+        <small>&copy; Made with 💚 by Lorenz</small>
+      </div>
     </footer>
   )
 }
