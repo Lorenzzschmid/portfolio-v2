f226c23596926ea09e9fc00fbd0ed6c7cee9b1d8
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-24 19:57:04 +0100

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,9 +5,14 @@ import React from 'react'
 const About = () => {
   return (
     <div className="about-container">
-        <h5>Hi, I am Lorenz</h5>
+        <h1>About</h1>
+
+        <p className='about-text'>
+          Hey! I am Lorenz, Fullstack Developer with a background in Philosophy and History. At the moment I am extending my knowledge in JavaScript and working on side projects created with React, SASS, Node.js, Express, MongoDB. 
+        </p>
 
         
+
     </div>
   )
 }
