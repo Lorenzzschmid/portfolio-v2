bbafdd004100a75f8ca23d8df8a1b6a31f9f73b2
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-24 15:44:13 +0100

    react-router and nav added

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,25 @@ import Nav from './components/nav/nav';
 import Portfolio from './components/portfolio/Portfolio';
 import Stack from './components/stack/Stack';
 import Footer from './components/footer/Footer';
+import { Routes, Route } from 'react-router-dom'; 
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Nav />
-      <About />
-      <Stack />
-      <Portfolio />
-      <Contact /> 
-      <Footer />     
+    <>
+    <Nav />
+      <div className="App">
+      <Routes>
+      <Route path='/home' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/stack' element={<Stack />} />
+      <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/footer' element={<Footer />} />
+    </Routes>
     </div>
+    </>
+    
+    
   );
 }
 
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -6,11 +6,11 @@ const Nav = () => {
     const [activeNav, setActiveNav] = useState('#'); 
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' && 'active'}>Home</a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' && 'active'}>About</a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' && 'active'}>Stack</a>
-      <a href="#links" onClick={() => setActiveNav('#links')} className={activeNav === '#links' && 'active'}>Portfolio</a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' && 'active'}>Contact</a>
+      <a href="/home" onClick={() => setActiveNav('#')} className={activeNav === '#' && 'active'}>Home</a>
+      <a href="/about" onClick={() => setActiveNav('/about')} className={activeNav === '#about' && 'active'}>About</a>
+      <a href="/stack" onClick={() => setActiveNav('/stack')} className={activeNav === '#experience' && 'active'}>Stack</a>
+      <a href="/portfolio" onClick={() => setActiveNav('/portfolio')} className={activeNav === '#links' && 'active'}>Portfolio</a>
+      <a href="/contact" onClick={() => setActiveNav('/contact')} className={activeNav === '#contact' && 'active'}>Contact</a>
     </nav>
   )
 }
