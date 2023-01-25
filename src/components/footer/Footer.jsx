4b7b88380a892388337a6be658fdb6c0a0750761
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-25 23:00:49 +0100

    footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,24 +7,24 @@ import {SiTryhackme} from 'react-icons/si';
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Lorenz Schmid</a>
+      <a href="#" className='footer-logo'>Lorenz Schmid</a>
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#Experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#sources">Sources</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/Experience">Experience</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="/sources">Sources</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
 
-      <div className="footer__socials">
+      <div className="footer-socials">
         <a href="https://twitter.com"><AiFillTwitterSquare/></a>
         <a href="https://www.codewars.com/users/Mighty%20Lo"><SiCodewars/></a>
         <a href="https://tryhackme.com/profile"><SiTryhackme/></a>
       </div>
       
-      <div className="footer__copyright">
+      <div className="footer-copyright">
         <small>&copy; Made with 💚 by Lorenz</small>
       </div>
     </footer>
