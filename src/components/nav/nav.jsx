65e43e28cf7fe1527182ddd84520446d9597a1c0
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-01-27 21:50:18 +0100

    active Navbar set

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -6,11 +6,11 @@ const Nav = () => {
     const [activeNav, setActiveNav] = useState('#'); 
   return (
     <nav>
-      <a href="/home" onClick={() => setActiveNav('#')} className={activeNav === '#' && 'active'}>Home</a>
-      <a href="/about" onClick={() => setActiveNav('/about')} className={activeNav === '#about' && 'active'}>About</a>
-      <a href="/stack" onClick={() => setActiveNav('/stack')} className={activeNav === '#experience' && 'active'}>Stack</a>
-      <a href="/portfolio" onClick={() => setActiveNav('/portfolio')} className={activeNav === '#links' && 'active'}>Portfolio</a>
-      <a href="/contact" onClick={() => setActiveNav('/contact')} className={activeNav === '#contact' && 'active'}>Contact</a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' && 'active'}>Home</a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' && 'active'}>About</a>
+      <a href="#stack" onClick={() => setActiveNav('#stack')} className={activeNav === '#stack' && 'active'}>Stack</a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' && 'active'}>Portfolio</a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' && 'active'}>Contact</a>
     </nav>
   )
 }
